abf60c6fb48e93bc6e01f5b60b2e66d4b70dedd8
David Ainslie <dainslie@gmail.com>
2016-03-18 14:49:17 +0000

    Version 1.3.1

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "1.3.0",
+      version := "1.3.1",
       scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
@@ -34,9 +34,9 @@ object Build extends Build {
       )
     )
     .settings(libraryDependencies ++= {
-      val `rtp-io-lib-version` = "1.7.0"
-      val `rtp-test-lib-version` = "1.2.0"
-      val `rtp-akka-lib-version` = "1.6.0"
+      val `rtp-io-lib-version` = "1.7.1"
+      val `rtp-test-lib-version` = "1.2.1"
+      val `rtp-akka-lib-version` = "1.6.1"
 
       Seq(
         "org.scalactic" %% "scalactic" % "2.2.4" withSources(),
